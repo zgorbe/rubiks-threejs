ad942f2088113490a8672f0ae201975d211b5931
Zoltan Gorbe <zoltan.gorbe@15five.com>
2020-12-04 09:18:52 +0100

    Add mouse event handlers

--- src/index.js
+++ src/index.js
@@ -25,28 +25,56 @@ window.addEventListener('resize', debounce(() => {
 
 // mouse and raycaster
 const mouse = new Vector2();
+const mouseStart = new Vector2();
+const raycaster = new Raycaster();
+
+window.addEventListener('mousedown', () => {
+  mouseStart.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouseStart.y = -(event.clientY / window.innerHeight) * 2 + 1;
+});
+window.addEventListener('mouseup', () => {
+  const mouseEnd = new Vector2();
+  mouseEnd.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouseEnd.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  if (mouseEnd.x !== 0 && mouseEnd.y !== 0) {
+    raycaster.setFromCamera(mouseStart, camera);
+    let intersects = raycaster.intersectObjects(cube.children);
+
+    if (intersects.length) {
+      console.log(intersects[0].object.position);
+
+      raycaster.setFromCamera(mouseEnd, camera);
+      intersects = raycaster.intersectObjects(cube.children);
+
+      if (intersects.length) {
+        console.log(intersects[0].object.position);
+      }
+    }
+  }
+});
+
 window.addEventListener('mousemove', () => {
   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
   mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  if (mouse.x !== 0 && mouse.y !== 0) {
+    raycaster.setFromCamera(mouse, camera);
+    let intersects = raycaster.intersectObjects(cube.children);
+
+    if (intersects.length) {
+      controls.enabled = false;
+    } else {
+      controls.enabled = true;
+    }
+  }
 });
-const raycaster = new Raycaster();
 
 // render
 const render = () => {
   requestAnimationFrame(render);
   // cube.rotateOnAxis(new THREE.Vector3(1, 1, 1).normalize(), 0.01);
   controls.update();
-
-  if (mouse.x !== 0 && mouse.y !== 0) {
-    raycaster.setFromCamera(mouse, camera);
-    // calculate objects intersecting the picking ray
-    const intersects = raycaster.intersectObjects(cube.children);
-
-    if (intersects.length) {
-      // const instanceId = intersects[0].instanceId;
-      console.log(intersects.map(item => item.object.position));
-    }
-  }
   renderer.render(scene, camera);
 };
 
